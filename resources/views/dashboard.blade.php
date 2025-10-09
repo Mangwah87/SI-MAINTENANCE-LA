31cc0cb11703aca19371ced899c16c3f0c15bb34
Super Key <kean3008@gmail.com>
2025-10-09 17:25:17 +0800

    Penambahan 14 Form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,15 +22,90 @@
                 </div>
 
                 <!-- ENV -->
-                <div class="bg-gradient-to-br from-yellow-50 to-orange-50 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                {{-- <div class="bg-gradient-to-br from-yellow-50 to-orange-50 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
                     <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
                        <i data-lucide="Container" class="w-12 h-12  text-yellow-500"></i>
                     </div>
-                    <span class="text-sm sm:text-base font-medium text-gray-700 text-center">Env Maintenance</span>
+                    <span class="text-sm sm:text-base font-medium text-gray-700 text-center"> Maintenance Env</span>
+                </div> --}}
+                <div class="bg-gradient-to-br from-blue-50 to-blue-100 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="ev-charger" class="w-12 h-12  text-blue-500"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-gray-700 text-center"> Maintenance Genset</span>
                 </div>
-
-                <h1>lanjutkan..... kawan sesuaikan dengan menu masing-masing</h1>
-
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="pc-case" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center"> Maintenance 1 Phase UPS</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="pc-case" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center"> Maintenance 3 Phase UPS</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="air-vent" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center"> Maintenance AC</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="arrow-up-wide-narrow" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center">Permohonan Tindak Lanjut Preventive Maintenance</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="arrow-up-narrow-wide" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center">Tindak Lanjut Preventive Maintenance</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="file-text" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center">Dokumentasi dan Pendataan Perangkat</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="calendar-check" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center">Jadwal Preventive Maintenance Sentral</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="move-right" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center">Preventive Maintenance Inverter -48VDC-220VAC</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="house" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center">Preventive Maintenance Ruang Shelter</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="arrow-left-right" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center">Preventive Maintenance Rectifier</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="zap" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center">Preventive Maintenance Petir dan Grounding</span>
+                </div>
+                <div class=" bg-blue-500 rounded-2xl p-6 flex flex-col items-center justify-center hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                    <div class="mb-4 transform group-hover:scale-110 transition-transform duration-300">
+                       <i data-lucide="cable" class="w-12 h-12  text-white"></i>
+                    </div>
+                    <span class="text-sm sm:text-base font-medium text-white text-center">Preventive Maintenance Instalasi Kabel dan Panel Distribusi</span>
+                </div>
             </div>
         </div>
     </div>
